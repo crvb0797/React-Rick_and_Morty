6489f798b54a53288ffd37f9c1043eceed0b63a3
Carlos Villatoro <crvb0797@gmail.com>
2023-02-05 19:33:37 -0600

    Configurando Github Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://crvb0797.github.io/React-Rick_and_Morty/",
   plugins: [react()],
 })
